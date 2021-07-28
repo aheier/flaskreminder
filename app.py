6493e3a54a330c32c0590ffe0ea4c0d16ef612cf
aheier <anthony.heier78@gmail.com>
2021-07-27 20:18:14 -0500

    add reminder page and form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for,render_template
+from flask import Flask, request, url_for,render_template, redirect
 from flask.templating import render_template
 from models import *
 from tests import *
@@ -31,10 +31,44 @@ def getReminders():
 
 @app.post('/addReminder')
 def addReminder():
-    remind = json.loads(request.data)
-    remind['startTime'] =  datetime.datetime.strptime(remind['startTime'], '%Y-%m-%d %H:%M:%S.%f')
-    remind['endTime'] =  datetime.datetime.strptime(remind['endTime'], '%Y-%m-%d %H:%M:%S.%f')
-    reminder = Reminder(**remind)
-    db.session.add(reminder)
-    db.session.commit() 
-    return json.loads(request.data)
+    remind = {}
+    if(request.is_json):
+        remind = json.loads(request.data)
+        remind['startTime'] =  datetime.datetime.strptime(remind['startTime'], '%Y-%m-%d %H:%M:%S.%f')
+        remind['endTime'] =  datetime.datetime.strptime(remind['endTime'], '%Y-%m-%d %H:%M:%S.%f')
+        reminder = Reminder(**remind)
+        db.session.add(reminder)
+        db.session.commit() 
+        return json.loads(request.data)
+    else:
+        print(request.form['text'])
+        print(request.form['startDate'])
+        print(request.form['endDate'])
+        remind['reminderText'] = request.form['text']
+        remind['startTime'] = datetime.datetime.strptime(request.form['startDate'], '%Y-%m-%dT%H:%M')
+        remind['endTime'] = datetime.datetime.strptime(request.form['endDate'], '%Y-%m-%dT%H:%M')
+        remind['isActive'] = True if (request.form['isActive'] == 'True') else False
+        print(remind['isActive'])
+        reminder = Reminder(**remind)
+        db.session.add(reminder)
+        db.session.commit() 
+        return redirect(url_for('getReminders'))
+        
+        
+
+
+@app.get('/addReminder')
+def addReminderform():
+    return render_template('reminderForm.html')
+
+
+@app.delete('/deleteReminder/<int:id>')
+def deleteReminder(id):
+    try:
+        reminder = Reminder.query.filter_by(id=id).first()
+        db.session.delete(reminder)
+        db.session.commit()
+    except Exception:
+        print(Exception)
+    finally:
+        return f'Deleted id: {reminder}'
